a85fff7d5d2f7993d76eb93f6232dc1977dbbc63
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2024-11-10 22:48:41 -0300

    first commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,20 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextResponse } from "next/server";
+
+const genAI = new GoogleGenerativeAI("AIzaSyCLnFI-5XVKeckGe9wx66LsBxo6upTMc7s");
+
+export async function POST(req: Request) {
+  try {
+    const { prompt } = await req.json();
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    return NextResponse.json({ message: text });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error procesando la solicitud" },
+      { status: 500 }
+    );
+  }
+}
