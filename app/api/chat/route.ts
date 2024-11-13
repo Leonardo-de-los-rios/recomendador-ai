a96e07e5885bbac1e4cbee713b7f32172e6ccaa1
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2024-11-13 18:19:00 -0300

    Update ChatComponent use model tuned

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,11 +10,23 @@ if (!process.env.GEMINI_API_KEY) {
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
+const context = `
+Eres un experto en recomendaciones de celulares. 
+Cada vez que recibas una consulta, debes responder con una lista de los 5 mejores celulares 
+que se ajusten a las necesidades especificadas en la consulta. 
+Incluye solo el modelo y una breve descripción de por qué es adecuado.
+`;
+
 export async function POST(req: Request) {
   try {
     const { prompt } = await req.json();
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-    const result = await model.generateContent(prompt);
+
+    const input = `${context}\n\nConsulta del usuario: ${prompt}\n\nRecomendación:`;
+
+    const model = genAI.getGenerativeModel({
+      model: "tunedModels/smartphonesv4-n7q1nczw35jf",
+    });
+    const result = await model.generateContent(input);
     const response = await result.response;
     const text = response.text();
     return NextResponse.json({ message: text });
