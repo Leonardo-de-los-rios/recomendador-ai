4b7229dbec0bb56a8e6084eca9617e4b1f774b72
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2024-11-14 01:22:50 -0300

    Gemini: fix component never used

--- app/gemini/page.tsx
+++ app/gemini/page.tsx
@@ -17,7 +17,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Select,
   SelectContent,
